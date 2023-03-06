ea87124035fffc8f5d6e978d31614e9e110c52d7
Pasindu Shavinda <pasindu.shv@gmail.com>
2023-03-06 13:14:05 +0530

    Firebase Database Configurations Done

---
+++ database/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1yBUSJQOeX-b-3WJkBZSJzHvUbo2sJ0A",
+    authDomain: "ctse-mobile-app.firebaseapp.com",
+    projectId: "ctse-mobile-app",
+    storageBucket: "ctse-mobile-app.appspot.com",
+    messagingSenderId: "652221257814",
+    appId: "1:652221257814:web:286081defb23ba902983f7"
+};
+
+// Initialize Firebase
+const fireDB = initializeApp(firebaseConfig);
+
+export default {
+    fireDB
+};
