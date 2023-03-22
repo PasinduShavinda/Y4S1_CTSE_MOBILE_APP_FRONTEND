5256c1c178ca845fe5c0ae00b156a2eaa9645d8b
Chamath <chamathkavvindya@gmail.com>
2023-03-22 20:17:44 +0530

    get nearby locations

--- screens/PetTraining/AllTrainings.js
+++ screens/PetTraining/AllTrainings.js
@@ -1,4 +1,11 @@
-import { View, StyleSheet, TouchableHighlight, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  Image,
+  Text,
+  Button,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { getAllTrainingsSub } from "../../services/PetTraining/trainingService";
 import Screen from "../../components/PetTraining/common/Screen";
@@ -6,18 +13,51 @@ import { FlatGrid } from "react-native-super-grid";
 import colors from "../../utils/colors";
 import StarRating from "../../components/PetTraining/StartRatingDisplay";
 import routes from "../../navigation/PetTraining/routes";
-
+import * as Location from "expo-location";
+import { getNearByPlaces } from "../../services/PetTraining/locationsNearBy";
 export default function AllTrainings({ navigation }) {
   const [listings, setListings] = useState([]);
-
+  const [location, setLocation] = useState(null);
+  const [locations, setLocations] = useState([]);
   useEffect(() => {
     getAll();
+    if (listings.length !== 0) {
+      async function getCurrentLocation() {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          return;
+        }
+
+        const { coords } = await Location.getCurrentPositionAsync({});
+        setLocation(coords);
+      }
+      getCurrentLocation();
+    }
   }, []);
+
+  const getNearBy = () => {
+    const list = [];
+    listings.forEach((item) => {
+      list.push(item.location);
+    });
+    //console.log(location)
+
+    const places = getNearByPlaces(list, location);
+    console.log(places);
+  };
+
   const getAll = () => {
     getAllTrainingsSub(setListings);
   };
   return (
     <Screen>
+      <Text>Training Available</Text>
+      <Button
+        title="Near by"
+        onPress={() => {
+          getNearBy();
+        }}
+      />
       <FlatGrid
         itemDimension={130}
         data={listings}
--- services/PetTraining/locationsNearBy.js
+++ services/PetTraining/locationsNearBy.js
@@ -1,30 +1,42 @@
-const locations = [
-  { latitude: 37.7749, longitude: -122.4194 },
-  { latitude: 37.7831, longitude: -122.4039 },
-  { latitude: 37.7875, longitude: -122.3964 },
-  // add more locations here
-];
-function distance(lat1, lng1, lat2, lng2) {
-  const earthRadius = 6371; // in kilometers
-  const dLat = ((lat2 - lat1) * Math.PI) / 180;
-  const dLng = ((lng2 - lng1) * Math.PI) / 180;
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLng / 2) *
-      Math.sin(dLng / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = earthRadius * c;
-  return distance; // in kilometers
+
+export function getNearByPlaces(locations, currentLocation) {
+  // const locations = [
+  //   { latitude: 37.7749, longitude: -122.4194 },
+  //   { latitude: 37.7831, longitude: -122.4039 },
+  //   { latitude: 37.7875, longitude: -122.3964 },
+  //   // add more locations here
+  // ];
+  
+
+  function distance(lat1, lng1, lat2, lng2) {
+    const earthRadius = 6371; // in kilometers
+    const dLat = ((lat2 - lat1) * Math.PI) / 180;
+    const dLng = ((lng2 - lng1) * Math.PI) / 180;
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLng / 2) *
+        Math.sin(dLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c;
+    return distance; // in kilometers
+  }
+
+  const nearbyLocations = locations.filter(
+    (location) =>
+      distance(
+        currentLocation.latitude,
+        currentLocation.longitude,
+        location.latitude,
+        location.longitude
+      ) <= 20
+  );
+
+  return nearbyLocations;
 }
 
-const nearbyLocations = locations.filter(
-  (location) =>
-    distance(currentLat, currentLng, location.latitude, location.longitude) <=
-    20
-);
-<MapView style={styles.map}>
+{/* <MapView style={styles.map}>
   {nearbyLocations.map((location) => (
     <Marker
       key={location.latitude + location.longitude}
@@ -41,4 +53,4 @@ const nearbyLocations = locations.filter(
       ).toFixed(2)} km`}
     />
   ))}
-</MapView>;
+</MapView>; */}
